44ae8840a4aad72c7b4c83bb29532842ef31354c
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-03-19 23:30:11 +0100

    init

---
+++ lib/paths.lua
@@ -0,0 +1,9 @@
+-- stores commonly used paths esp. for graphics
+
+--====================================================================================================
+--PATHS
+--====================================================================================================
+
+ei_fueler_path = "__exotic-industries-fueler__/"
+
+ei_fueler_graphics_path = ei_fueler_path.."graphics/"
