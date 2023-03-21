d925fb0ba00cf126b56a4b6328f10cb19610180e
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-03-21 22:37:22 +0100

    v0.1.0

--- settings.lua
+++ settings.lua
@@ -12,7 +12,7 @@ data:extend({
         name = "ei_fueler_range",
         type = "int-setting",
         setting_type = "startup",
-        default_value = "8",
+        default_value = "10",
         minimum_value = 1,
         maximum_value = 100,
         order  = "b1",
